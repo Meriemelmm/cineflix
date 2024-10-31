f5bdad6ee9f4037ffb982caeadddacb0fb1419a0
meriem el mecaniqui <melecaniqui@gmail.com>
2024-10-31 21:43:52 +0100

    fff

--- home.js
+++ home.js
@@ -1,13 +1,34 @@
-function filtercards(){
-    const searchInput = document.querySelector("#search");
+const searchInput = document.querySelector("#search");
+searchInput.addEventListener("input", filterCards);
+
+function filterCards() {
     const value = searchInput.value.toLowerCase();
-    const cards =  document.querySelectorAll(".more") ;
-    cards.forEach((card)=>{
+    const cards = document.querySelectorAll(".more");
+    cards.forEach((card) => {
         let text = card.textContent.toLowerCase();
         if(text.includes(value)){
             card.style.display = "";
         }else{
             card.style.display = "none";
         }
-    })
-}
+       
+    });
+}//
+//const scrolling = document.querySelector('.films');
+//scrolling.animate([
+    //{ transform: 'translateX(0)' },
+    //{ transform: 'translateX(-100vh)'
+        
+
+    // }//
+//], {
+   // duration: 10000, // Durée de l'animation en millisecondes
+   // iterations: Infinity // Répète l'animation indéfiniment
+//});//
+//scrolling.scrollBy({
+    //Top: 0,
+    //Left: 100,
+   // Behaviour: smooth});//
+
+
+  
--- test.js
+++ test.js
@@ -1,41 +1,20 @@
+const buttons = document.querySelectorAll('[data-slider-button]')
 
- //document.write( '<h1> hellooooo </h1>');//
- //console.log('meriem');//
- //window.alert('el mecaniqui');//
- //connaitre erreur : par console quand je fais inspecter  apres console il affichr ou est erreur
- //par exemple main.js:5 c ad l erreur existe dans la ligne 5//
- // on utilise ; pour separe l2awamir //
- //data type:primitive:string, nulber, booleen , null=nothing, undefined: n existe pas//
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.sliderButton === 'next' ? 1 : -1
+        const slides = button
+        .closest('[data-slider]')
+        .querySelector('[data-slides]')
 
+    const activeSlide = slides.querySelector('[data-active]')
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if  (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-
-/*  on peut utiliser le dolar quand  on fait declaration de variable 
- var name;
- name='meriem';
- var:variable qui peut changer   n importe temps 
- on peut faire aussi declaration et ajouter une valeuur: var name='hello';
- let: peut faire de claration et retourne a autre ligne et fait values  
- mais pour const: il ne peut faire par exemple declaration et retourne a un autre ligne pour donner la valeur il doit fairee comme ca const x=10;*/
- //const x=10;
- //let y;
- //y=15;
- //var z;
- //z=14;
- //console.log( x);
- //console.log(y);
- //console.log(z);//
- //pour damj deux elements par exemples  on fait +  dans console log 
- //console.log('ahmed  kndiro had espace ila bghina matkonsh ahmed las9a f 28'+ 28)//
- let x=10;
- let y='ahmed    ';
- console.log(y+x);
- var num1=15;
- var num2=14;
- console.log(`my name is ${ y}`);
- console.log(num1 - num2);
- console.log(num1 / num2);
- console.log(num1 % num2);
- console.log(num1 * num2);
-
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+    })
+})
 
   
